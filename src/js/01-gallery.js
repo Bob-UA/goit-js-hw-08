ba647903b510d38abd3b376bb7513d05e2fc493e
Bob-UA <120073426+Bob-UA@users.noreply.github.com>
2023-04-28 13:39:31 -0700

    1-task_completed

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,38 @@
 // Add imports above this line
-import { galleryItems } from './gallery-items';
+import { galleryItems } from './gallery-items.js';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const gallery = document.querySelector(".gallery");
+
+const markupCards = createGalleryCardsMarckup(galleryItems);
+gallery.insertAdjacentHTML("afterbegin", markupCards);
+
+function createGalleryCardsMarckup(elements) {
+  return elements
+    .map(({ preview, original, description }) => {
+      return `<li class="gallery__item">
+          <a class="gallery__link" href="${original}">
+            <img
+              class="gallery__image"
+              src="${preview}"
+              alt="${description}"
+              title="${description}"
+
+            />
+          </a>
+        </li>`;
+    })
+    .join('');
+}
+    let gal = new SimpleLightbox('.gallery a');
+    gal.on('show.simplelightbox', function () {});
+    gal.on('error.simplelightbox', function (e) {
+      console.log(e);
+    });
+
+
+
 // Change code below this line
 
 console.log(galleryItems);
