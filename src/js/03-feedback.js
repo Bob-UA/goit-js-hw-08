b5c7ae292d96dbeb913a46dab144380daa1a84a9
Bob-UA <120073426+Bob-UA@users.noreply.github.com>
2023-04-30 11:17:37 -0700

    npm_run_build

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,25 @@
+const form = document.querySelector('.feedback-form');
+const takeFormData = localStorage.getItem('feedback-form-state');
 
+const handleSubmit = e => {
+    e.preventDefault();
+    console.log(e);
+  const {
+    elements: { email, message },
+  } = e.currentTarget;
+
+  if (email.value === '' || message.value === '') {
+    return alert('Please fill in all the fields!');
+  }
+  const formData = new FormData(e.currentTarget);
+    formData.forEach((value, name) => {
+    console.log(`${name}, ${value}`);
+    
+    takeFormData.setItem('feedback-form-state', `${name}, ${value}`);
+    e.currentTarget.reset();
+  });
+};
+
+console.log(takeFormData);
+
+form.addEventListener('submit', handleSubmit);
