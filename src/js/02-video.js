8e93bea6f41805d774d3aef3f2c32c41dc72b7ed
Bob-UA <120073426+Bob-UA@users.noreply.github.com>
2023-05-03 13:06:37 -0700

    2-task_fixed

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -6,40 +6,16 @@ const player = new Player(iframe);
 const currentTime = localStorage.getItem('videoplayer-current-time');
 
 
-player.on('play', function () {
-  console.log('played the video!');
-});
-
-player.getVideoTitle().then(function (title) {
-  console.log('title:', title);
-});
-
 const onPlay = function (data) {
-  console.log(data);
   localStorage.setItem('videoplayer-current-time', `${data.seconds}`);
-  console.log(localStorage.getItem('videoplayer-current-time'));
-  return localStorage.getItem('videoplayer-current-time');
 };
 
 
+fromSavedTime();
+function fromSavedTime() {
+  if (currentTime) {
+player.setCurrentTime(currentTime); 
+  }
+}
 
-
-player
-  .setCurrentTime(currentTime)
-  .then(function (seconds) {
-    // seconds = the actual time that the player seeked to
-  })
-  .catch(function (error) {
-    switch (error.name) {
-      case 'RangeError':
-        // the time was less than 0 or greater than the video’s duration
-        break;
-
-      default:
-        // some other error occurred
-        break;
-    }
-  });
-
-  
 player.on('timeupdate', trottle(onPlay, 1000));
